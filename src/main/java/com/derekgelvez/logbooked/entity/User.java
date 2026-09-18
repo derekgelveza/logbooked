4fb474574c3d899ebbeb5d7d992c8b0fde7c608e
derekgelveza <gel24002@byui.edu>
2026-09-18 16:41:11 -0600

    Created User entity module

---
+++ src/main/java/com/derekgelvez/logbooked/entity/User.java
@@ -0,0 +1,35 @@
+package com.derekgelvez.logbooked.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+public class User {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+
+
+}
